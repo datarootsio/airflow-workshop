c24fb9d372436232f80ded3a62dce7c8db14d62b
Nicolas Gelders <Nicolas.Gelders@vrt.be>
2022-08-08 23:26:44 +0000

    BakeOnePizzaAndWait basics

--- airflow/plugins/pizzeria_plugin/operators/bake_one_pizza_and_wait.py
+++ airflow/plugins/pizzeria_plugin/operators/bake_one_pizza_and_wait.py
@@ -1,3 +1,8 @@
+import os
+import json
+
+import requests
+
 from airflow.models.baseoperator import BaseOperator
 
 
@@ -6,6 +11,15 @@ class BakeOnePizzaAndWait(BaseOperator):
         super().__init__(**kwargs)
 
     def execute(self, context):
-        message = "Hello world"
-        print(message)
-        return message
+        pizzeria_webserver_url = os.environ["PIZZERIA_WEBSERVER"]
+
+        resp = requests.post(f"http://{pizzeria_webserver_url}/api/pizza/bake")
+
+        if resp.status_code != 200:
+            raise Exception(
+                f"Something went wrong calling pizza api:\n{json.dumps(resp.json())}"
+            )
+
+        order_id = resp.json()["data"]["orderId"]
+
+        return order_id
