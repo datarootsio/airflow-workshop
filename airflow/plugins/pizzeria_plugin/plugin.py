09ac86b15468ead4a91e0ffb93ccd6679d1bf560
Nicolas Gelders <Nicolas.Gelders@vrt.be>
2022-08-11 07:51:54 +0000

    remove timetable from plugin

--- airflow/plugins/pizzeria_plugin/plugin.py
+++ airflow/plugins/pizzeria_plugin/plugin.py
@@ -1,11 +1,7 @@
 from airflow.plugins_manager import AirflowPlugin
 
-from pizzeria_plugin.timetables.my_timetable import (
-    EveryDayAtMidnightExceptWeekendsAndHolidaysTimetable,
-)
-
 
 class PizzeriaPlugin(AirflowPlugin):
     name = "pizzeria_plugin"
 
-    timetables = [EveryDayAtMidnightExceptWeekendsAndHolidaysTimetable]
+    timetables = []
